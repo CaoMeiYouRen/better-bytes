9c912d7eb2a1a2ae331fc4af97cda5cc04cfbb3b
CaoMeiYouRen <996881204@qq.com>
2024-04-27 20:35:00 +0800

    feat: 完成 parse 逻辑；优化 小数 和 大数字的精度处理

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,9 @@ export const KILO_BINARY_BYTE_UNITS = ['B', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', '
  */
 export const KILOBYTE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
 
+export const KILO_BINARY_BYTE_BINARY = 1024
+export const KILOBYTE_UINARY = 1000
+
 /**
  * kilobinary = 2^10 ; kilo = 10^3
  */
@@ -38,21 +41,25 @@ export type Options = {
  * @date 2024-04-27
  * @export
  * @param data
+ * @param [options={}]
  */
 export function format(data: number | bigint, options: Options = {}): string {
+    if (typeof data !== 'number' && typeof data !== 'bigint') {
+        throw new Error('Data must be a number or bigint')
+    }
     if (data < 0) {
         throw new Error('Data must be greater than or equal to 0')
     }
     const { standard = 'kilobinary', decimal = 2, unitSeparator = ' ' } = options
     const units = standard === 'kilobinary' ? KILO_BINARY_BYTE_UNITS : KILOBYTE_UNITS
-    const binary = standard === 'kilobinary' ? 1024 : 1000
-    const bigIntBinary = BigInt(binary)
+    const binary = standard === 'kilobinary' ? KILO_BINARY_BYTE_BINARY : KILOBYTE_UINARY
     let i = 0
     let value: number | bigint = data
 
     if (typeof value === 'bigint') {
-        while (value >= bigIntBinary && i < units.length - 1) {
-            value /= bigIntBinary
+        const bigIntbinary = BigInt(binary)
+        while (value >= bigIntbinary && i < units.length - 1) {
+            value /= bigIntbinary
             i++
         }
     } else {
@@ -68,6 +75,54 @@ export function format(data: number | bigint, options: Options = {}): string {
     return `${value.toFixed(decimal)}${unitSeparator}${units[i]}`
 }
 
-export function parse() {
+export const PARSE_REG_EXP = /^((-|\+)?(\d+(?:\.\d+)?)) *((k|m|g|t|p|e|z|y)?i?b)$/i
 
+/**
+ * Parse the string value into an integer in bytes.
+ * If no unit is given, it is assumed the value is in bytes.
+ * 将字符串值解析为以字节为单位的整数。
+ * 如果没有给出单位，则假定该值以字节为单位。
+ * @author CaoMeiYouRen
+ * @date 2024-04-27
+ * @export
+ * @param data
+ */
+export function parse(data: string): number | bigint | null {
+    if (typeof data !== 'string') {
+        throw new Error('Data must be a string')
+    }
+    const results = PARSE_REG_EXP.exec(data)
+    const floatValue = parseFloat(results?.[1] || data)
+    const unit = results?.[4]?.toLowerCase()
+    if (isNaN(floatValue) || floatValue < 0) {
+        return null
+    }
+    if (unit === '') {
+        return floatValue
+    }
+    let i = 0
+    let standard: StandardType
+    i = KILO_BINARY_BYTE_UNITS.findIndex((e) => e.toLowerCase() === unit)
+    if (i >= 0) {
+        standard = 'kilobinary'
+    } else {
+        i = KILOBYTE_UNITS.findIndex((e) => e.toLowerCase() === unit)
+        if (i >= 0) {
+            standard = 'kilo'
+        }
+    }
+    if (!standard) {
+        return null
+    }
+    const binary = standard === 'kilobinary' ? KILO_BINARY_BYTE_BINARY : KILOBYTE_UINARY
+    let result: number | bigint = floatValue
+    for (let j = 0; j < i; j++) {
+        const nextResult = typeof result === 'bigint' ? result * BigInt(binary) : result * binary
+        if (nextResult > Number.MAX_SAFE_INTEGER) {
+            result = BigInt(result) * BigInt(binary)
+        } else {
+            result = nextResult
+        }
+    }
+    return result
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,5 @@
-/* eslint-disable no-loss-of-precision */
-/* eslint-disable @typescript-eslint/no-loss-of-precision */
-import { format } from '../src'
+/* eslint-disable no-loss-of-precision, @typescript-eslint/no-loss-of-precision */
+import { format, KILO_BINARY_BYTE_UNITS, KILOBYTE_UNITS, parse } from '../src'
 
 describe('format', () => {
     it('should format bytes correctly for numbers', () => {
@@ -41,15 +40,6 @@ describe('format', () => {
         expect(format(18446744073709551615)).toBe('16.00 EiB')
         expect(format(18446744073709551616n)).toBe('16 EiB')
         expect(format(36893488147419103232n)).toBe('32 EiB')
-    })
-
-    it('should throw an error when data is negative', () => {
-        expect(() => format(-1)).toThrow('Data must be greater than or equal to 0')
-        expect(() => format(-100)).toThrow('Data must be greater than or equal to 0')
-        expect(() => format(-1024)).toThrow('Data must be greater than or equal to 0')
-        expect(() => format(-1n)).toThrow('Data must be greater than or equal to 0')
-        expect(() => format(-100n)).toThrow('Data must be greater than or equal to 0')
-        expect(() => format(-1024n)).toThrow('Data must be greater than or equal to 0')
     })
 
     it('should format bytes correctly with kilobinary standard', () => {
@@ -101,5 +91,105 @@ describe('format', () => {
         expect(format(1000, { standard: 'kilo', unitSeparator: '' })).toBe('1.00KB')
     })
 
+    it('should throw an error when data is negative', () => {
+        expect(() => format(-1)).toThrow('Data must be greater than or equal to 0')
+        expect(() => format(-100)).toThrow('Data must be greater than or equal to 0')
+        expect(() => format(-1024)).toThrow('Data must be greater than or equal to 0')
+        expect(() => format(-1n)).toThrow('Data must be greater than or equal to 0')
+        expect(() => format(-100n)).toThrow('Data must be greater than or equal to 0')
+        expect(() => format(-1024n)).toThrow('Data must be greater than or equal to 0')
+    })
+
+    it('should throw an error if data is not a number or bigint', () => {
+        expect(() => format('invalid' as any)).toThrow('Data must be a number or bigint')
+        expect(() => format(true as any)).toThrow('Data must be a number or bigint')
+        expect(() => format({} as any)).toThrow('Data must be a number or bigint')
+    })
+
 })
 
+describe('parse', () => {
+    test('should parse valid binary units', () => {
+        expect(parse('1B')).toBe(1)
+        expect(parse('1KiB')).toBe(1024)
+        expect(parse('1MiB')).toBe(1048576)
+        expect(parse('1GiB')).toBe(1073741824)
+        expect(parse('1TiB')).toBe(1099511627776)
+        expect(parse('1PiB')).toBe(1125899906842624)
+        expect(parse('1EiB')).toBe(1152921504606846976n)
+        expect(parse('1ZiB')).toBe(1180591620717411303424n)
+        expect(parse('1YiB')).toBe(1208925819614629174706176n)
+    })
+
+    test('should parse valid decimal units', () => {
+        expect(parse('1B')).toBe(1)
+        expect(parse('1KB')).toBe(1000)
+        expect(parse('1MB')).toBe(1000000)
+        expect(parse('1GB')).toBe(1000000000)
+        expect(parse('1TB')).toBe(1000000000000)
+        expect(parse('1PB')).toBe(1000000000000000)
+        expect(parse('1EB')).toBe(1000000000000000000n)
+        expect(parse('1ZB')).toBe(1000000000000000000000n)
+        expect(parse('1YB')).toBe(1000000000000000000000000n)
+    })
+
+    test('should parse decimal values', () => {
+        expect(parse('1.5KiB')).toBe(1536)
+        expect(parse('1.5KB')).toBe(1500)
+    })
+
+    test('should handle case insensitive units', () => {
+        expect(parse('1kib')).toBe(1024)
+        expect(parse('1KB')).toBe(1000)
+    })
+
+    test('should handle positive and negative values', () => {
+        expect(parse('+1KiB')).toBe(1024)
+        expect(parse('-1KiB')).toBeNull()
+    })
+
+    test('should return null for invalid input', () => {
+        expect(parse('invalid')).toBeNull()
+        expect(parse('1XB')).toBeNull()
+    })
+
+    test('should throw error for non-string input', () => {
+        expect(() => parse(123 as unknown as string)).toThrow('Data must be a string')
+    })
+
+    test('should return bigint for values greater than MAX_SAFE_INTEGER', () => {
+        const maxSafeInteger = BigInt(Number.MAX_SAFE_INTEGER)
+        const binaryUnit = KILO_BINARY_BYTE_UNITS[KILO_BINARY_BYTE_UNITS.length - 1]
+        const decimalUnit = KILOBYTE_UNITS[KILOBYTE_UNITS.length - 1]
+
+        expect(parse(`${maxSafeInteger + 1n}${binaryUnit}`)).toBe(10889035741470030830827987437816582766592n)
+        expect(parse(`${maxSafeInteger + 1n}${decimalUnit}`)).toBe(9007199254740992000000000000000000000000n)
+    })
+
+    test('should return number for values within MAX_SAFE_INTEGER', () => {
+        const maxSafeInteger = Number.MAX_SAFE_INTEGER
+        const binaryUnit = KILO_BINARY_BYTE_UNITS[KILO_BINARY_BYTE_UNITS.length - 2]
+        const decimalUnit = KILOBYTE_UNITS[KILOBYTE_UNITS.length - 2]
+
+        expect(parse(`${maxSafeInteger}${binaryUnit}`)).toBe(10633823966279325802638835764831453184n)
+        expect(parse(`${maxSafeInteger}${decimalUnit}`)).toBe(9007199254740991000000000000000000000n)
+    })
+
+    test('should handle values with many decimal places', () => {
+        expect(parse('1.123456789012345KiB')).toBe(1150.4197519486413)
+        expect(parse('1.123456789012345KB')).toBe(1123.456789012345)
+    })
+
+    test('should handle values with many decimal places and return bigint when necessary', () => {
+        const maxSafeInteger = Number.MAX_SAFE_INTEGER
+        const binaryUnit = KILO_BINARY_BYTE_UNITS[KILO_BINARY_BYTE_UNITS.length - 1]
+        const decimalUnit = KILOBYTE_UNITS[KILOBYTE_UNITS.length - 1]
+
+        const valueWithManyDecimals = `${maxSafeInteger}.123456789012345`
+        const expectedBinaryResult = 10889035741470029621902167823187408060416n // BigInt(Math.floor(parseFloat(valueWithManyDecimals) * 1024 ** (KILO_BINARY_BYTE_UNITS.length - 1)))
+        const expectedDecimalResult = 9007199254740991000000000000000000000000n  // BigInt(Math.floor(parseFloat(valueWithManyDecimals) * 1000 ** (KILOBYTE_UNITS.length - 1)))
+
+        expect(parse(`${valueWithManyDecimals}${binaryUnit}`)).toBe(expectedBinaryResult)
+        expect(parse(`${valueWithManyDecimals}${decimalUnit}`)).toBe(expectedDecimalResult)
+    })
+})
